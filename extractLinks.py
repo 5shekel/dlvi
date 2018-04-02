5d26af824a6cc30d49db57b5dfe4ffefe1a15445
yair <yair99@gmail.com>
2018-04-02 17:17:27 +0200

    Update extractLinks.py

--- extractLinks.py
+++ extractLinks.py
@@ -1,16 +1,7 @@
 #!/usr/bin/env python2
 import bs4,sys
 
-example='''
-<div class ="foo">
-     <a href ="http://example.com"> </a>
-     <a href ="http://example2.com"> Title here </a>
-</div>
 
-<div class ="foo">
-     <a href ="http://example3.com"> </a>
-     <a href ="http://example4.com"> Title 2 here </a>
-'''
 
 
 with open(sys.argv[1], 'r') as f:
@@ -18,5 +9,5 @@ with open(sys.argv[1], 'r') as f:
 
 soup = bs4.BeautifulSoup(webpage, "lxml")
 for a in soup.findAll('a', href=True):
-    #print "https://vimeo.com", a['href']
-    print ("https://vimeo.com%s" % a['href'])
+   Print ("https://vimeo.com%s" % a['href'])
+
